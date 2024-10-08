67e09b55f1086d3d1dde1da2dcae054395ceab65
cordero-agustin <agustincorderoacc@gmail.com>
2024-10-07 23:08:31 -0300

    confirmación monto

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const divisas = [
     dolar=1155,
     pesos = prompt('Cantidad de pesos a convertir'),
     euro=1317]
+//Confirmación del monto
+confirm('Desea convertir'+pesos +'ARS a dolares?')
 //Resultado de la conversión
 let resultado = (pesos/dolar).toFixed(2);
 //Alert por si se ingresa un valor no numérico
